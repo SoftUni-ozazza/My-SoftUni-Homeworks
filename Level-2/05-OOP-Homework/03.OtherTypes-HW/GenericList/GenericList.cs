d22e1c56dbe3cab7ddccad15e42cd1db5ab322f7
Teodora Vacheva <ozvacheva@gmail.com>
2014-09-30 23:02:11 +0300

    update task 3 at HW 3.Other-Types

---
+++ Level-2/05-OOP-Homework/03.OtherTypes-HW/GenericList/GenericList.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GenericList<T> where T : IComparable<T>
+{
+    const int DefaultCapacity = 16;
+
+    private T[] elements;
+    private int count = 0;
+
+    public GenericList(int capacity = DefaultCapacity)
+    {
+        elements = new T[capacity];
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.count;
+        }
+    }
+    public int Capacity
+    {
+        get
+        {
+            return this.elements.Length;
+        }
+    }
+
+    public void Add(T element)
+    {
+        //if (this.Count >= this.Capacity)
+        //{
+        //    throw new IndexOutOfRangeException(String.Format("The capacity of {0} /was/ exceeded.", elements.Length));
+        //}
+        if (count >= this.Capacity)
+        {
+            Expand();
+        }
+        this.elements[this.Count] = element;
+        count++;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException(String.Format("Invalid index. Index {0} out of range.", index));
+            }
+            T result = elements[index];
+            return result;
+        }
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= count)
+        {
+            throw new ArgumentOutOfRangeException(String.Format("Invalid index. Index {0} out of range.", index));
+        }
+
+        for (int i = index; i < this.Count - 1; i++)
+        {
+            elements[i] = elements[i + 1];
+        }
+        count--;
+        elements[count] = default(T);
+    }
+
+    public void InsertAt(T element, int index)
+    {
+        if (index < 0 || index >= count)
+        {
+            throw new ArgumentOutOfRangeException(String.Format("Invalid index. Index {0} out of range.", index));
+        }
+
+        if (count >= this.Capacity)
+        {
+            Expand();
+        }
+
+        for (int i = this.Count; i > index; i--)
+        {
+            this.elements[i] = this.elements[i - 1];
+        }
+        count++;
+        this.elements[index] = element;
+    }
+
+    public void Clear()
+    {
+        count = 0;
+        this.elements = new T[this.Capacity];
+    }
+
+    public int FindIndexOf(T element)
+    {
+        for (int i = 0; i < this.Count; i++)
+        {
+            if (this.elements[i].Equals(element))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public bool Contains(T element)
+    {
+        for (int i = 0; i < this.count; i++)
+        {
+            if (this.elements[i].CompareTo(element) > 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T Min()
+    {
+        T resultMin = this.elements[0];
+        for (int i = 1; i < this.Count; i++)
+        {
+            if (this.elements[i].CompareTo(resultMin) < 0)
+            {
+                resultMin = this.elements[i];
+            }
+        }
+        return resultMin;
+    }
+
+    public T Max()
+    {
+        T resultMax = this.elements[0];
+        for (int i = 1; i < this.Count; i++)
+        {
+            if (this.elements[i].CompareTo(resultMax) > 0)
+            {
+                resultMax = this.elements[i];
+            }
+        }
+        return resultMax;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < this.Count; i++)
+        {
+            if (i == this.Count - 1)
+            {
+                result.Append(this.elements[i]);
+            }
+            else
+            {
+                result.Append(this.elements[i] + ", ");
+            }
+        }
+        return result.ToString();
+    }
+
+    private void Expand()
+    {
+        T[] expandArray = new T[this.Capacity * 2];
+        Array.Copy(elements, expandArray, this.Capacity);
+        this.elements = expandArray;
+    }
+
+}
---
+++ Level-2/05-OOP-Homework/03.OtherTypes-HW/GenericList/GenericListMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    class GenericListMain
+    {
+        public static void Main()
+        {
+            GenericList<int> numbers = new GenericList<int>();
+            for (int i = 0; i < 20; i++)
+            {
+                numbers.Add(i + 1);
+            }
+            Console.WriteLine(numbers);
+            Console.WriteLine(numbers.Min());
+            Console.WriteLine(numbers.Max());
+            Console.WriteLine("Contains 100 -> {0}", numbers.Contains(100));
+            Console.WriteLine("Contains 3 -> {0}", numbers.Contains(3));
+            Console.WriteLine("Index of 1 is {0}", numbers.FindIndexOf(1));
+            numbers.RemoveAt(4);
+            Console.WriteLine("Remove value at index 4 -> {0}", numbers);
+            numbers.InsertAt(4, 12);
+            Console.WriteLine("Insert 4 at index 12 -> {0}", numbers);
+            numbers.Clear();
+            Console.WriteLine("Empty {0}", numbers);
+            numbers.Add(5);
+            Console.WriteLine(numbers);
+        }
+    }
+}
